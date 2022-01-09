8d1691c01ec7c98cbabc44bb3c598e5a02f57c91
Jakub Morawski <jakub.morawski8@gmail.com>
2022-01-09 16:56:14 +0100

    feature register-module refactoring

---
+++ gym-core-app/src/app/services/auth/register.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { RegisterCredentials } from '../../models/register-credentials';
+
+const registerURL: string = environment.url + "signup";
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  
+  constructor(private httpClient: HttpClient) { }
+
+  register(credentials: RegisterCredentials): Observable<any> {
+    return this.httpClient.post(registerURL, credentials, httpOptions);
+  }
+}
